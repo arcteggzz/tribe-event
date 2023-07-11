95ba6b7b44fd1c6742f91e051cb128ed88fb779e
Oghenetega Esedere <arcteggzz@gmail.com>
2023-07-11 13:20:05 +0100

    feat: created navbar and navigation elements

--- src/utils/routePaths.ts
+++ src/utils/routePaths.ts
@@ -1,6 +1,11 @@
 const routePaths = {
   SPLASHPAGE: "/",
   HOMEPAGE: "/home",
+  ABOUT_US: "/about",
+  GET_A_TICKET: "/get-a-tickect",
+  EVENT_LISTINGS: "/event-listings",
+  LOGIN: "/login",
+  SIGNUP: "/signup",
 };
 
 export default routePaths;
